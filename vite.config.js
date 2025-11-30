82dc65d13cb9bc62f4ef39cb408f1e8d18c12f23
Pavani <pavanikolavennu16@gmail.com>
2025-11-30 19:33:49 +0530

    Added GitHub Pages deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-        import { defineConfig } from 'vite';
-        import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-        export default defineConfig({
-          plugins: [react()],
-          base: '/FEDF_Project_HeritageConnect/',
-        });
+export default defineConfig({
+  plugins: [react()],
+  base: '/HeritageConnect/'   // MUST match repo name exactly
+})
